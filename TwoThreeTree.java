2aa26d2015a32ff1646f5f6b53b527ed3f824370
pisarpe1 <47826168+pisarpe1@users.noreply.github.com>
2019-12-18 14:19:59 +0100

    Update TwoThreeTree.java

--- TwoThreeTree.java
+++ TwoThreeTree.java
@@ -60,7 +60,11 @@ class TwoThreeTree<T extends Comparable<T>> implements Iterable
         return traverse.ordered.iterator();           // Delegujte úlohu iterátoru na vestavěný iterátor propojeného seznamu.
     }
 
+
+
     //-----------------------------------------Inner Iterator Class-------------------------------------------------//
+    
+    
 
     private class Traversal                            // vytvoření třídy Traversal
     {
@@ -89,11 +93,13 @@ class TwoThreeTree<T extends Comparable<T>> implements Iterable
             }
         }
     }
+    
 
     //-------------------------------------------INNER CLASS NODE---------------------------------------------------//
     
     
-    private class Node                                     // vytvoření třídy Node
+    
+    private class Node                                                       // vytvoření třídy Node uzly
     {
         ArrayList<T> keys = new ArrayList<>(3);
         ArrayList<Node> children = new ArrayList<>(4);
@@ -105,13 +111,13 @@ class TwoThreeTree<T extends Comparable<T>> implements Iterable
 
         private void addKey(T x)
         {
-            this.keys.add(x);                                 // vlož x do poslední pozice
+            this.keys.add(x);                                                // vlož x do poslední pozice
             int lastIndex = this.keys.size()-1;
-            for (int i = 0; i < lastIndex; i++)               //  Opakovaně zkontrolujte klíč v indexu i, pokud je větší než klíč v indexu keySize  
+            for (int i = 0; i < lastIndex; i++)                              //  Opakovaně zkontrolujte klíč v indexu i, pokud je větší než klíč v indexu keySize  
             {
                 if (this.keys.get(i).compareTo(this.keys.get(lastIndex)) > 0)
                 {
-                    T temp = this.keys.get(i);                //  Pokud ano, vyměňte je a zvyšte i
+                    T temp = this.keys.get(i);                               //  Pokud ano, vyměňte je a zvyšte i
                     this.keys.set(i, this.keys.get(lastIndex));
                     this.keys.set(lastIndex, temp);
                 }
@@ -120,55 +126,56 @@ class TwoThreeTree<T extends Comparable<T>> implements Iterable
 
         private boolean insert(T x)
         {
-            if (this.keys.contains(x))                                               // Pokud uzel obsahuje klíč, vraťte false
+            if (this.keys.contains(x))                                        // Pokud uzel obsahuje klíč, vraťte false
                 return false;
 
             int i = 0;
-            while (i < this.keys.size() && x.compareTo(this.keys.get(i)) > 0)        // Najděte správného potomka, kterého chcete vložit
+            while (i < this.keys.size() && x.compareTo(this.keys.get(i)) > 0) // Najděte správného potomka, kterého chcete vložit
                 i++;
             boolean childWasSplit;
-
-            if (i < this.children.size())                               // Uzel není list, takže mohu rekurzivně vložit
+    
+            if (i < this.children.size())                                     // Uzel není list, takže mohu rekurzivně vložit
                 childWasSplit = this.children.get(i).insert(x);
-            else                                                        // Je to list, stačí přidat klíč a pak zkontrolovat, zda je třeba rozdělit
+            else                                                              // Je to list, stačí přidat klíč a pak zkontrolovat, zda je třeba rozdělit
             {
                 this.addKey(x);
-                size++;                                                 // Klíč nebyl duplikát a mohl být vložen do stromu, proto zvětšujte jeho velikost
+                        size++;                                               // Klíč nebyl duplikát a mohl být vložen do stromu, proto zvětšujte jeho velikost
                 {
-                    this.splitify();                                    // Rozdělte uzel a vraťte true, abyste informovali rodiče
-                    return true;                                        // Potomek byl rozdělen
+                    this.splitify();                                          // Rozdělte uzel a vraťte true, abyste informovali rodiče
+                    return true;                                              // Potomek byl rozdělen
                 }
                 return false;
             }
 
             if (childWasSplit)
-            {                                                           // Potomek byl rozdělen a rodič může být 2-uzel nebo 3-uzel
-                Node tempChild = this.children.get(i);                  // Kopie rozděleného potomka, protože se změní přepsáním potomků rodičů
-                this.addKey(tempChild.keys.get(0));                     // Přesunutí klíče od potomka k rodiči"
-                this.children.set(i, tempChild.children.get(0));        // Aktuální potomek je nahrazen levým potomkem
-                this.children.add(i+1, tempChild.children.get(1));      // Přidejte pravého potomka do dalšího indexu. ArrayList provede řazení automaticky.
-                if (this.children.size() == 4)                          // Uzel je opravdu plný, byl to 3-uzel a nyní se stal 4-uzlem, takže to bylo
+            {                                                                  // Potomek byl rozdělen a rodič může být 2-uzel nebo 3-uzel
+                Node tempChild = this.children.get(i);                         // Kopie rozděleného potomka, protože se změní přepsáním potomků rodičů
+                this.addKey(tempChild.keys.get(0));                            // Přesunutí klíče od potomka k rodiči"
+                this.children.set(i, tempChild.children.get(0));               // Aktuální potomek je nahrazen levým potomkem
+                this.children.add(i+1, tempChild.children.get(1));             // Přidejte pravého potomka do dalšího indexu. ArrayList provede řazení automaticky.
+                if (this.children.size() == 4)                                 // Uzel je opravdu plný, byl to 3-uzel a nyní se stal 4-uzlem, takže to bylo
                 {
-                    this.splitify();                                    // potřebuje přerozdělit své potomky vytvořením uzlů ze sebe
+                    this.splitify();                                           // potřebuje přerozdělit své potomky vytvořením uzlů ze sebe
                     return true;
                 }
-            }                                                           // Byl to 2-uzel, takže se potomci rozštěpeného potomka staly potomky rodičů a
-            return false;                                               // je to klíč upravený uvnitř nadřazeného tak, aby se stal 3-uzlem
+            }                                                                  // Byl to 2-uzel, takže se potomci rozštěpeného potomka staly potomky rodičů a
+            return false;                                                       
+                                                                               // je to klíč upravený uvnitř nadřazeného tak, aby se stal 3-uzlem
         }
 
         private void splitify()
         {
             Node left = new Node(this.keys.get(0));
             Node right = new Node(this.keys.get(2));
-            if (this.children.size() == 4)                      // Pokud je uzel přeplněný, vyrovnejte vše
-            {                                                   // tím, že vlevo dáte nejvýše dva uzly
-                left.children.add(this.children.get(0));        // a nejvíce dva uzly k pravému potomku
+            if (this.children.size() == 4)                                     // Pokud je uzel přeplněný, vyrovnejte vše
+            {                                                                  // tím, že vlevo dáte nejvýše dva uzly
+                left.children.add(this.children.get(0));                       // a nejvíce dva uzly k pravému potomku
                 left.children.add(this.children.get(1));
                 right.children.add(this.children.get(2));
                 right.children.add(this.children.get(3));
             }
             this.children.clear();
-            this.children.add(left);                            // nastavení nového pravého a levého potomka
+            this.children.add(left);                                          // nastavení nového pravého a levého potomka
             this.children.add(right);
 
             T tempKey = this.keys.get(1);                       // Také klíče byly předány potomkům
